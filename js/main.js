5b76de6d2b708f985beb80cdbdaeb59603ef21f5
Daniel Kling <dan@717studios.com>
2015-02-08 11:33:45 -0500

    Home page
    Home page without responsive styles

---
+++ js/main.js
@@ -0,0 +1,20 @@
+$(".nav").stickyPanel();
+$('.flexslider-1').flexslider({
+	controlNav: false,
+	directionNav: false,
+	animation: "fade",
+	animationLoop: true, 
+	reverse: false,
+	direction: "vertical",
+	randomize: true
+});
+$('.flexslider-2').flexslider({
+	controlNav: false,
+	directionNav: false,
+	animation: "fade",
+	animationLoop: true, 
+	reverse: true,
+	direction: "vertical",
+	initDelay: 800,
+	randomize: true, 
+});
